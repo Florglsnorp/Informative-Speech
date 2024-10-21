bb96f1bb05c7ee66e2b065e615f4410aeb67e4d8
Florglsnorp <nicholaslink234@gmail.com>
2024-10-21 17:44:27 -0400

    Better Slideshow

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-let slideIndex = 0;
-showSlides();
+document.querySelectorAll('.navigation a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
-function plusSlides(n) {
-    slideIndex += n;
-    showSlides();
-}
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    if (slideIndex >= slides.length) { slideIndex = 0; }
-    if (slideIndex < 0) { slideIndex = slides.length - 1; }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex].style.display = "block";
-}
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
